4e046918ef1ca1150d3b7ec6c5834b4fd1934df0
Dawood Ayaz <dawoodayaz18@gmail.com>
2026-01-16 03:56:23 +0500

    create navbar.

--- src/components/dashboard/DashboardPage.tsx
+++ src/components/dashboard/DashboardPage.tsx
@@ -1,30 +1,63 @@
 import React, {useState} from "react";
+import { Link } from "react-router-dom";
+import dashboard from '../../assets/dashboard.png'
+import income from '../../assets/salary.png'
+import expense from '../../assets/spending.png'
+import forecast from '../../assets/forecast.png'
+import settings from '../../assets/settings.png'
+import profile from '../../assets/profile.png'
 
 const Dashboard: React.FC = () => {
-  const toggleNavbar = useState<boolean>(true);
+  const [navbarOpen, setnavbarOpen] = useState<boolean>(true);
+  const data = [
+                { month: "Jan", balance: 1000 },
+                { month: "Feb", balance: 800 },
+                { month: "Mar", balance: 400 },
+                { month: "Apr", balance: 100 },
+                { month: "May", balance: -200 },
+              ];
 
   return (
     <div className="flex h-screen bg-gray-100">
       {/* Sidebar */}
-      <aside className="w-56 bg-white shadow-md p-2 flex flex-col">
-        {/* User */}
-        <div className="flex flex-col items-center mb-8">
+        {navbarOpen 
+          ?
+          <aside className="w-12 bg-white shadow-md p-2 flex flex-col">  
+          <div className="flex flex-col items-center mb-8">
+          <div className="w-10 h-10 p-5 rounded-full bg-blue-500 flex items-center justify-center text-white text-2xl mb-10">
+            😊
+          </div>
+          <button onClick={() => setnavbarOpen(!navbarOpen)} className="p-5">☰</button>
+          <div className="flex flex-col gap-4">
+            <Link to='/' className="hover:bg-gray-200"><img src={dashboard} className="w-6 h-6"></img></Link>
+            <Link to='/' className="hover:bg-gray-200"><img src={income} className="w-6 h-6"></img></Link>
+            <Link to='/' className="hover:bg-gray-200"><img src={expense} className="w-6 h-6"></img></Link>
+            <Link to='/' className="hover:bg-gray-200"><img src={forecast} className="w-6 h-6"></img></Link>
+            <Link to='/' className="hover:bg-gray-200"><img src={settings} className="w-6 h-6"></img></Link>
+            <Link to='/' className="hover:bg-gray-200"><img src={profile} className="w-6 h-6"></img></Link>
+          </div>
+          </div>
+          </aside>
+          :
+          <aside className="w-56 bg-white shadow-md p-2 flex flex-col">
+          <div className="flex flex-col items-center mb-8">
           <div className="w-20 h-20 rounded-full bg-blue-500 flex items-center justify-center text-white text-2xl mb-2">
             😊
           </div>
-          <span className="font-semibold">Name</span>
+          <span className="font-semibold m-4">Name</span>
+          <button onClick={() => setnavbarOpen(!navbarOpen)} className="p-5">☰</button>
+          <nav className="flex flex-col gap-2">
+            <button className="text-left px-4 py-2 rounded hover:bg-gray-200 hover:font-semibold">Dashboard</button>
+            <button className="text-left px-4 py-2 rounded hover:bg-gray-200 hover:font-semibold">Income</button>
+            <button className="text-left px-4 py-2 rounded hover:bg-gray-200 hover:font-semibold">Expense</button>
+            <button className="text-left px-4 py-2 rounded hover:bg-gray-200 hover:font-semibold">Cash-Flow Forecast</button>
+            <button className="text-left px-4 py-2 rounded hover:bg-gray-200 hover:font-semibold">Settings</button>
+            <button className="text-left px-4 py-2 rounded hover:bg-gray-200 hover:font-semibold">Profile</button>
+          </nav>
+        
         </div>
-
-        {/* Navigation */}
-        <nav className="flex flex-col gap-2">
-          <button className="text-left px-4 py-2 rounded hover:bg-gray-200 hover:font-semibold">Dashboard</button>
-          <button className="text-left px-4 py-2 rounded hover:bg-gray-200 hover:font-semibold">Income</button>
-          <button className="text-left px-4 py-2 rounded hover:bg-gray-200 hover:font-semibold">Expense</button>
-          <button className="text-left px-4 py-2 rounded hover:bg-gray-200 hover:font-semibold">Cash-Flow Forecast</button>
-          <button className="text-left px-4 py-2 rounded hover:bg-gray-200 hover:font-semibold">Settings</button>
-          <button className="text-left px-4 py-2 rounded hover:bg-gray-200 hover:font-semibold">Profile</button>
-        </nav>
       </aside>
+      }
 
       {/* Main Content */}
       <main className="flex-1 p-6 overflow-y-auto">
