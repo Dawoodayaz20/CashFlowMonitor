88368d6fa9bdeaaf7bd6b613ded49cbb7962d292
Dawood Ayaz <dawoodayaz18@gmail.com>
2026-02-17 21:12:08 +0500

    Add cookie-parser for creating httpOnly cookies.

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -1,9 +1,9 @@
-// src/server.js
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('../config/db');
 const authRoutes = require('./routes/authRoutes')
+const cookieParser = require('cookie-parser');
 
 dotenv.config();
 connectDB();
@@ -13,6 +13,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cookieParser())
 
 app.use('/api/auth', authRoutes);
 
