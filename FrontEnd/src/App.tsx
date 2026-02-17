3ab119cd355b986736154b00603846af34b3c40d
Dawood Ayaz <dawoodayaz18@gmail.com>
2026-02-18 04:16:08 +0500

    Create protected routes

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,15 +1,30 @@
-import './App.css'
-import './index.css'
-import Dashboard from './components/dashboard/DashboardPage'
-import Auth from './components/authentication/AuthPage'
+import './App.css';
+import './index.css';
+import Dashboard from './components/dashboard/DashboardPage';
+import Auth from './components/authentication/AuthPage';
+import { Routes, BrowserRouter, Route, Navigate } from 'react-router-dom';
+import useAuthStore from './store/useAuthStore';
+import type React from 'react';
 
 function App() {
 
   return (
-    <>
-      <Auth />
-    </>
+        <Routes>
+          <Route path='/' element={<Auth />} />
+          <Route path='/dashboard' element={<ProtectedRoute><Dashboard /></ProtectedRoute>}/>
+        </Routes>
   )
 }
+
+function ProtectedRoute({children} : {children: React.ReactNode}) {
+  const { user } = useAuthStore();
+  
+  if (!user) {
+    return <Navigate to="/" replace />;
+  }
+  
+  return <>{children}</>;
+}
+
 
 export default App
