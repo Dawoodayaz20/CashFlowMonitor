a1c1a151168d67f2c9d6d7f034c9baae57583360
Dawood Ayaz <dawoodayaz18@gmail.com>
2026-02-18 23:38:58 +0500

    Protect App with user authentication.

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import './index.css';
+import { useEffect } from 'react';
 import Dashboard from './components/dashboard/DashboardPage';
 import Auth from './components/authentication/AuthPage';
 import { Routes, BrowserRouter, Route, Navigate } from 'react-router-dom';
@@ -7,23 +8,40 @@ import useAuthStore from './store/useAuthStore';
 import type React from 'react';
 
 function App() {
+  const { user, clearUser } = useAuthStore();
 
-  return (
-        <Routes>
-          <Route path='/' element={<Auth />} />
-          <Route path='/dashboard' element={<ProtectedRoute><Dashboard /></ProtectedRoute>}/>
-        </Routes>
-  )
-}
-
-function ProtectedRoute({children} : {children: React.ReactNode}) {
-  const { user } = useAuthStore();
-  
+  function ProtectedRoute({children} : {children: React.ReactNode}) {
   if (!user) {
     return <Navigate to="/" replace />;
   }
-  
   return <>{children}</>;
+  }
+
+  useEffect(() => {
+    const verifyAuth = async () => {
+      try {
+        const res = await fetch('http://localhost:5000/api/auth/verify',{
+          credentials: 'include'
+        });
+
+        if (!res.ok){
+          clearUser();
+        }
+      }
+      catch(error){
+       clearUser();
+      }
+    }
+
+    if(user) verifyAuth();
+  }, [])
+
+  return (
+        <Routes>
+          <Route path='/' element={user ? <Navigate to="/dashboard" /> : <Auth />} />
+          <Route path='/dashboard' element={<ProtectedRoute><Dashboard /></ProtectedRoute>}/>
+        </Routes>
+  )
 }
 
 
