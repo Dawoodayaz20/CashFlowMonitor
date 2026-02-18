b46a4739ff9ffb87f6e4f06412c237154fdbf739
Dawood Ayaz <dawoodayaz18@gmail.com>
2026-02-18 12:36:09 +0500

    Use zustand lib for storing state.

---
+++ FrontEnd/src/store/useAuthStore.ts
@@ -0,0 +1,29 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+interface User {
+    id: string;
+    name: string;
+    email: string
+}
+
+interface AuthState  {
+    user: User | null;
+    setUser: (user: User) => void;
+    clearUser: () => void;
+}
+
+const useAuthStore = create<AuthState>()(
+  persist(
+    (set) => ({
+      user: null,
+      setUser: (user) => set({ user }),
+      clearUser: () => set({ user: null }),
+    }),
+    {
+      name: 'auth-storage', // 👈 key in localStorage
+    }
+  )
+);
+
+export default useAuthStore;
