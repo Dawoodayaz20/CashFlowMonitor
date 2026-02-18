a5a4024c7c9c22472eee53c3a455ee3a72ae54c4
Dawood Ayaz <dawoodayaz18@gmail.com>
2026-02-19 01:29:24 +0500

    create sign out route and cookie validation route.

--- BackEnd/src/routes/authRoutes.js
+++ BackEnd/src/routes/authRoutes.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../controllers/authController');
+const { register, login, logout } = require('../controllers/authController');
+const authMiddleware = require('../middleware/authMiddleware');
 
 // POST /api/auth/register
 router.post('/register', register);
-router.post('/login', login)
+
+
+router.post('/login', login);
+
+router.get('/verify', authMiddleware, (req, res) => {
+    res.json({ valid: true, userId: req.userId });
+});
+
+router.post('/logout', logout);
 
 module.exports = router;
