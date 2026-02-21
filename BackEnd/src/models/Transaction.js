c78340bd299ef1450567f8f590c4417c71712b2e
Dawood Ayaz <dawoodayaz18@gmail.com>
2026-02-22 02:23:17 +0500

    create schema for saving transactional data.

--- BackEnd/src/models/Transaction.js
+++ BackEnd/src/models/Transaction.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose')
 
 const transactionSchema = new mongoose.Schema({
   userId: { 
@@ -46,4 +46,4 @@ const transactionSchema = new mongoose.Schema({
     timestamps: true 
 });
 
-export default mongoose.model('Transaction', transactionSchema);
+module.exports = mongoose.model('Transaction', transactionSchema);
