b1bc03b401fcba06ece7b74c42eb09300a97d7ae
Dawood Ayaz <dawoodayaz18@gmail.com>
2026-03-06 16:51:11 +0500

    update import for authPage

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -2,7 +2,7 @@ import './App.css';
 import './index.css';
 import { useEffect } from 'react';
 import Dashboard from './pages/Dashboard/DashboardPage';
-import Auth from './components/authentication/AuthPage';
+import Auth from '../src/pages/authPage/AuthPage';
 import TransactionPage from './components/TransactionPage/transactionPage';
 import ForecastPage from './pages/ForecastPage/forecastPage';
 import SettingsPage from './pages/SettingsPage/settingsPage';
