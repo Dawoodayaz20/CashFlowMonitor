ff8b46966f2861ccf4d6e6c94a67e6e8421c7bd0
Dawood Ayaz <dawoodayaz18@gmail.com>
2026-03-12 01:03:36 +0500

    create login method with google.

--- FrontEnd/src/authentication/googleAuth.tsx
+++ FrontEnd/src/authentication/googleAuth.tsx
@@ -1,20 +1,18 @@
-import { useGoogleLogin } from '@react-oauth/google';
 import useAuthStore from '../store/useAuthStore';
 
-export const GoogleSignIn = async (accessToken: string, navigate: any) => {
-    const { setUser } = useAuthStore(); 
+export const GoogleSignIn = async (accessToken: string) => {
+    const { setUser } = useAuthStore.getState(); 
 
     try{
-        const response = await fetch('', {
+        const response = await fetch('http://localhost:5000/api/auth/google', {
             method: 'POST',
-            headers: { 'Content-Type': 'application-json'},
+            headers: { 'Content-Type': 'application/json'},
             credentials: 'include',
             body: JSON.stringify({ token: accessToken})
         })
         const data = await response.json();
         if (response.ok) {
         setUser(data.user);
-        navigate('/dashboard');
         }
         return data;
     } catch (error) {
