3fd5ef38e60574dca99e92ec56cd2faecd46a419
Dawood Ayaz <dawoodayaz18@gmail.com>
2026-03-17 01:35:01 +0500

    Add route to save settings in dB.

--- BackEnd/src/routes/settingsRoutes.js
+++ BackEnd/src/routes/settingsRoutes.js
@@ -1,10 +1,7 @@
-const { Resend } = require('resend');
 const express = require('express');
 const router = express.Router();
 const { getSettings, updateSettings } = require('../controllers/settingsController');
 const authMiddleware = require('../middleware/authMiddleware');
-
-const resend = new Resend(process.env.RESEND_API_KEY);
 
 router.use(authMiddleware);
 
